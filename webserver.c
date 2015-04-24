15225767744cc0f41b3fdb19573b14cb48049fe6
etingi01 <etingi01@cs.ucy.ac.cy>
2015-04-24 17:33:07 +0300

    get plirofories

--- webserver.c
+++ webserver.c
@@ -6,14 +6,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "webserver.h"
-
-
 int WORKERS;
 int PORT;
 //messages
-char ok[16]  = "HTTP/1.1 200 OK";
-char notFound[23]  = "HTTP/1.1 404 NOT FOUND";
-char notImpl[29]  = "HTTP/1.1 501 NOT IMPLEMENTED";
+char ok[16] = "HTTP/1.1 200 OK\r\n";
+char notFound[23] = "HTTP/1.1 404 NOT FOUND\r\n";
+char notImpl[29] = "HTTP/1.1 501 NOT IMPLEMENTED\r\n";
 
 char* giveContentType(char *arxeio){
 	char temp[18];
@@ -25,25 +23,29 @@ char* giveContentType(char *arxeio){
 		split=strtok(NULL,".");
 	}
 	printf("Type: %s\n" word);
-	if ((strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)){
+	if ((strcmp(word,"txt")==0)||(strcmp(word,"sed")==0)||(strcmp(word,"awk")==0)||(strcmp(word,'c')==0)||(strcmp(word,'h')==0)){
 		temp="text/plain";
 	}else{
-	  if ((strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)){
- 		temp="text/html";				
-          }else{
-	    	  if ((strcmp(word,"txt")==0)||(strcmp(word,"txt")==0)){ 
-			temp= "image/jpeg";
+	if ((strcmp(word,"html")==0)||(strcmp(word,"htm")==0)){
+		temp="text/html";
+	}else{
+		if ((strcmp(word,"jpeg")==0)||(strcmp(word,"jpg")==0)){
+		temp= "image/jpeg";
+		}else{
+			if (strcmp(word,"gif")==0){
+			temp="image/gif";
+			}else{
+				if (strcmp(word,"pdf")==0){
+				temp="application/pdf";
+				}
+			}	
 
-		  }
-	   }
-
+		}
+	}
 	}
 	char* str=malloc(18);
 	strcpy(str, temp);
-
 }
-
-
 void getRequest(char* arxeio){
 	FILE *fp;
 	char *line = NULL;
@@ -51,16 +53,31 @@ void getRequest(char* arxeio){
 	if (fp==NULL){
 		write("%s\n", notFound);
 	}else{
-	  
-	  size_t len = 0;
-	  ssize_t read;
-	  while ((read = getline(&line, &len, fp)) != -1) {
+		size_t len = 0;
+		ssize_t read;
+		while ((read = getline(&line, &len, fp)) != -1) {
 		write(read);
-	  }
+		}
+		write("Server: Marios and Evanthia Server\r\n");
+		fclose(output_file);
+		fp = fopen(arxeio, "r");
+		char nextChar = getc(fp);
+		int numCharacters = 0;
+
+		while (nextChar != EOF) {
+		    //Do something else, like collect statistics
+		    numCharacters++;
+		    nextChar = getc(fp);
+		}
+		write("Content Length:");
+		write(numCharacters);
+		write("\r\n");
+		write("Content Type:");
+		char* tipos= giveContentType(arxeio);
+		write(tipos);
+		write("\r\n");
 	}
 }
-
-
 void readConfig() {
 	FILE *fp;
 	char *line = NULL;
@@ -82,20 +99,18 @@ void readConfig() {
 			tmp[7] = '\0';
 			PORT = atoi(tmp);
 			printf("PORT : %d \n", PORT);
+			}
 		}
-	}
 	fclose(fp);
 	if (line)
-		free(line);
+	free(line);
 }
 int main() {
 	readConfig();
-
 	int create_socket, new_socket;
 	socklen_t addrlen;
 	int bufsize = 1024;
 	char *buffer = malloc(bufsize);
-
 	struct sockaddr_in address;
 	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
 		printf("The socket was created\n");
@@ -103,7 +118,6 @@ int main() {
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
-
 	while (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) != 0){
 		printf("try bind\n");
 	}
@@ -130,31 +144,25 @@ int main() {
 		char* requestFile;
 		char* connectionType;
 		split=strtok(request," ");
-
 		while(split != NULL){
 			word=split;
 			printf("%s\n", word);
-			if ((strcmp(split,"GET")==0)||(strcmp(split,"HEAD")==0)||(strcmp(split,"DELETE")==0)){
-				split=strtok(NULL," ");
-				requestFile=split;
-				printf("Requested File: %s\n", requestFile);
+		if ((strcmp(split,"GET")==0)||(strcmp(split,"HEAD")==0)||(strcmp(split,"DELETE")==0)){
+			split=strtok(NULL," ");
+			requestFile=split;
+			printf("Requested File: %s\n", requestFile);
 			}
-
 			split=strtok(NULL," ");
 		}
 		printf("%s\n", request);
 		if (strcmp(request,"GET")==0){
 			printf("The Request was GET\n");
-			
-
 		}
 		if (strcmp(request,"HEAD")==0){
 			printf("The Request was HEAD\n");
-
 		}
 		if (strcmp(request,"DELETE")==0){
 			printf("The Request was DELETE\n");
-
 		}
 		write(new_socket, "hello world\n", 12);
 		sleep(2);
