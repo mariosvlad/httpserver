036bf9534a1ebb603c313b0e16eb94f984a32c81
marios <marios.vladimirou@gmail.com>
2015-05-06 02:26:20 +0300

    deleted backup files, reformated test

--- web/testFiles/client.c
+++ web/testFiles/client.c
@@ -9,56 +9,63 @@
 
 
 /* Client with Internet stream sockets */
-main(int argc, char *argv[])
-{
-int port, sock, serverlen; char buf[256*8];
-struct sockaddr_in server;
-struct sockaddr *serverptr;
-struct hostent *rem;
+main(int argc, char *argv[]) {
+    int port, sock, serverlen;
+    char buf[256 * 8];
+    struct sockaddr_in server;
+    struct sockaddr *serverptr;
+    struct hostent *rem;
 /* Are server's host name and port number given? */
-if (argc < 3) {
-printf("Please give host name and port number\n"); exit(1);
-}
+    if (argc < 3) {
+        printf("Please give host name and port number\n");
+        exit(1);
+    }
 /* Create TCP/IP socket */
-if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-perror("socket"); exit(1);
-}
-if ((rem = gethostbyname(argv[1])) == NULL) {
-herror("gethostbyname"); exit(1);
-}
-port = atoi(argv[2]); /* Convert port number to integer */
-server.sin_family = PF_INET; /* Internet domain */
-bcopy((char *) rem -> h_addr, (char *) &server.sin_addr,
-rem -> h_length);
-server.sin_port = htons(port); /*Server's Internet address and port*/
-serverptr = (struct sockaddr *) &server;
-serverlen = sizeof(server);
-FILE *fp;
-if((fp=fopen(argv[3],"r"))==NULL){
-printf("File not found!");
-return 1;
-}
-if (connect(sock, serverptr, serverlen) < 0) { /* Request Connect */
-perror("connect"); exit(1); }
-printf("Requested connection to host %s port %d\n", argv[1], port);
-bzero(buf, sizeof buf); /* Initialize buffer */
-while (!feof(fp))
-fgets(&buf[strlen(buf)], sizeof buf, fp); /* Read all the file */
-buf[strlen(buf)] = '\r';
-buf[strlen(buf)+1] = '\n';
-buf[strlen(buf)+2] = '\0';
-printf("Sending:\n%s\n",buf);
-if (write(sock, buf, sizeof buf) < 0) { /* Send message */
-perror("write"); exit(1);
-}
-bzero(buf, sizeof buf); /* Initialize buffer */
-sleep(2);
-if (read(sock, buf, sizeof buf) < 0) { /* Receive message */
-perror("read"); exit(1);
-}
-close(sock); /* Close socket */
-printf("Got:\n%s",buf);
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    if ((rem = gethostbyname(argv[1])) == NULL) {
+        herror("gethostbyname");
+        exit(1);
+    }
+    port = atoi(argv[2]); /* Convert port number to integer */
+    server.sin_family = PF_INET; /* Internet domain */
+    bcopy((char *) rem->h_addr, (char *) &server.sin_addr,
+          rem->h_length);
+    server.sin_port = htons(port); /*Server's Internet address and port*/
+    serverptr = (struct sockaddr *) &server;
+    serverlen = sizeof(server);
+    FILE *fp;
+    if ((fp = fopen(argv[3], "r")) == NULL) {
+        printf("File not found!");
+        return 1;
+    }
+    if (connect(sock, serverptr, serverlen) < 0) { /* Request Connect */
+        perror("connect");
+        exit(1);
+    }
+    printf("Requested connection to host %s port %d\n", argv[1], port);
+    bzero(buf, sizeof buf); /* Initialize buffer */
+    while (!feof(fp))
+        fgets(&buf[strlen(buf)], sizeof buf, fp); /* Read all the file */
+    buf[strlen(buf)] = '\r';
+    buf[strlen(buf) + 1] = '\n';
+    buf[strlen(buf) + 2] = '\0';
+    printf("Sending:\n%s\n", buf);
+    if (write(sock, buf, sizeof buf) < 0) { /* Send message */
+        perror("write");
+        exit(1);
+    }
+    bzero(buf, sizeof buf); /* Initialize buffer */
+    sleep(2);
+    if (read(sock, buf, sizeof buf) < 0) { /* Receive message */
+        perror("read");
+        exit(1);
+    }
+    close(sock); /* Close socket */
+    printf("Got:\n%s", buf);
 
 
-exit(0);
+    exit(0);
 }
